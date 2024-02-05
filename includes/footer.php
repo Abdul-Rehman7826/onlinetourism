2ee637da8252c863a9845286c632071551e92ecb
AbdulRehman <112686679+Abdul-Rehman7826@users.noreply.github.com>
2024-02-05 21:38:08 +0500

    new file:   change-password.php 	new file:   check_availability.php 	new file:   css/animate.css 	new file:   css/bootstrap.css 	new file:   css/font-awesome.css 	new file:   css/jquery-ui.css 	new file:   css/style.css 	new file:   enquiry.php 	new file:   fonts/FontAwesome.otf 	new file:   fonts/fontawesome-webfont.eot 	new file:   fonts/fontawesome-webfont.svg 	new file:   fonts/fontawesome-webfont.ttf 	new file:   fonts/fontawesome-webfont.woff 	new file:   fonts/glyphicons-halflings-regular.eot 	new file:   fonts/glyphicons-halflings-regular.ttf 	new file:   fonts/glyphicons-halflings-regular.woff 	new file:   fonts/glyphicons-halflings-regular.woff2 	new file:   forgot-password.php 	new file:   images/1.jpg 	new file:   images/1.png 	new file:   images/13.jpg 	new file:   images/2.png 	new file:   images/3.jpg 	new file:   images/4.jpg 	new file:   images/5.jpg 	new file:   images/6.jpg 	new file:   images/7.jpg 	new file:   images/Kashmir.jpg 	new file:   images/about.jpg 	new file:   images/app.png 	new file:   images/bus.png 	new file:   images/c1.jpg 	new file:   images/c2.jpg 	new file:   images/c3.jpg 	new file:   images/c4.jpg 	new file:   images/c5.jpg 	new file:   images/date-icon.png 	new file:   images/desert.jpg 	new file:   images/dot.png 	new file:   images/footer.jpg 	new file:   images/h1.jpg 	new file:   images/h2.jpg 	new file:   images/h3.jpg 	new file:   images/h4.jpg 	new file:   images/h5.jpg 	new file:   images/home.jpg 	new file:   images/img-sprite.png 	new file:   images/map1.png 	new file:   images/mark1.png 	new file:   images/seat-1.png 	new file:   images/seat-2.png 	new file:   images/seat-3.png 	new file:   images/seat-4.png 	new file:   images/seat-5.png 	new file:   images/seat.png 	new file:   images/signup.jpg 	new file:   images/social.png 	new file:   images/t1.jpg 	new file:   images/t2.jpg 	new file:   images/t3.jpg 	new file:   images/t4.jpg 	new file:   images/t5.jpg 	new file:   includes/config.php 	new file:   includes/footer.php 	new file:   includes/header.php 	new file:   includes/signin.php 	new file:   includes/signup.php 	new file:   includes/write-us.php 	new file:   index.php 	new file:   js/bootstrap.min.js 	new file:   js/jquery-1.12.0.min.js 	new file:   js/jquery-ui.js 	new file:   js/wow.min.js 	new file:   logout.php 	new file:   pacakgeimages/14287acc-b5cb-46db-b8b4-e3ffe652fc0d.png 	new file:   pacakgeimages/Kashmir.jpg 	new file:   pacakgeimages/complaints.jpg 	new file:   pacakgeimages/coorg-hill-station1.jpg 	new file:   pacakgeimages/images.jpg 	new file:   pacakgeimages/mamp-pro-logo-big.png 	new file:   pacakgeimages/pexels-abhilash-mishra-1539700.jpg 	new file:   pacakgeimages/phpgurukul-1.png 	new file:   package-details.php 	new file:   package-list.php 	new file:   page.php 	new file:   profile.php 	new file:   sql file/tms.sql 	new file:   tour-history.php

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com" target="blank"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://www.Twitter.com" target="blank"><span>Twitter</span></a></li>
+				<li><a class="googleplus" href="https://www.Gmail.com" target="blank"><span>Gmail</span></a></li>
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2022 TGS . All Rights Reserved </p>
+	</div>
+</div>
